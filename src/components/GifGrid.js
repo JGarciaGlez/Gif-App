a374009e6441ebae97bf85f268efded8ae621dab
javier <elteliblejvi@gmail.com>
2022-05-04 13:05:15 +0200

    update styles

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,29 +1,21 @@
-import React from 'react';
-import { useFetchGifs } from '../hooks/useFetchGifs';
-import { GifGridItem } from './GifGridItem';
+import React from "react";
+import { useFetchGifs } from "../hooks/useFetchGifs";
+import { GifGridItem } from "./GifGridItem";
 
 export const GifGrid = ({ category }) => {
+  const { data: images, loading } = useFetchGifs(category);
 
-    const { data:images, loading } = useFetchGifs( category );
+  return (
+    <>
+      <h3 className="text-center"> {category} </h3>
 
-    return (
-        <>
-            <h3 className="animate__animated animate__fadeIn text-center"> { category } </h3>
+      {loading && <p>Loading</p>}
 
-            { loading && <p className="animate__animated animate__flash">Loading</p> }
-
-            <div className="d-flex justify-content-center mx-auto text-center row align-items-center ">
-                
-                {
-                    images.map( img => (
-                        <GifGridItem 
-                            key={ img.id }
-                            { ...img }
-                        />
-                    ))
-                }
-            
-            </div>
-        </>
-    )
-}
+      <div className="d-flex justify-content-center mx-auto text-center row align-items-center ">
+        {images.map((img) => (
+          <GifGridItem key={img.id} {...img} />
+        ))}
+      </div>
+    </>
+  );
+};
